c798b7450c82eebd71d782dc4809392b8da7e2c7
HARI PRASATH MN <148082831+hariprakash-max@users.noreply.github.com>
2024-01-02 18:53:25 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from time import strftime
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+root = tk.Tk()
+root.title("Digital Clock")
+root.geometry("400x200")
+root.resizable(False, False)
+label = tk.Label(root, font=('calibri', 40, 'bold'), background='black', foreground='white')
+label.pack(anchor='center')
+time()
+root.mainloop()
